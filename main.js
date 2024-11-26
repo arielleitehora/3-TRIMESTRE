6a6e1d9fe572b116988957a7a3f2f0af7df382c6
Ariel Hora <126189837+arielleitehora@users.noreply.github.com>
2024-11-26 09:07:08 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+criaCartao(
+    "Programação",
+    "O que é Python?",
+    "O Python é uma linguagem de programação"
+)
+
+criaCartao(
+    "Geografia",
+    "Qual a capital da França?",
+    "A capital da França é Paris"
+)
+
+criaCartao(
+    "Esporte",
+    "Qual e o time do povo?",
+    "O time do povo e o Corinthias"
+)
